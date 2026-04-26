4203c4b4269277b88a08759c5674d7b0d35140bd
pranay-bhatkar <pranaybhatkar81@gmail.com>
2026-04-27 01:36:58 +0530

    LinkedList

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -89,29 +89,214 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MAX_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev : i = size - 2;
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+
+    }
+
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void deleteNthFromLast(int n) {
+        // cal size
+        int size = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            temp = temp.next;
+            size++;
+        }
+
+        if (n == size) {
+            head = head.next; // removeFirst
+            return;
+        }
+
+        // size - n
+        int i = 1;
+        int iToFind = size - n;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    // slow - fast approach
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+
+        }
+        return slow; // slow is my middle node;
+    }
+
+    public boolean isPalindrome() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // 1. find mid
+        Node midNode = findMid(head);
+
+        // 2. reverse 2n half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node right = prev; // RH head
+        Node left = head; // LH head
+
+        // 3. check left half & right half
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
         LinkedList l1 = new LinkedList();
 //        l1.head = new Node(1);
 //        l1.head.next = new Node(2);
+//
+//        l1.print();
+////         add first
+//        l1.addFirst(2);
+//        l1.print();
+//        l1.addFirst(1);
+//        l1.print();
+////        add last
+//        l1.addLast(3);
+//        l1.print();
+//        l1.addLast(4);
+//        l1.print();
+//
+//        l1.addMiddle(2, 9);
+//        l1.print();
+//
+//        System.out.println("LL size : " + l1.size);
+//        l1.removeFirst();
+//        l1.print();
+//
+//        l1.removeLast();
+//        l1.print();
+//        System.out.println(l1.size);
+//
+//        System.out.println(l1.itrSearch(3));
+//        System.out.println(l1.itrSearch(10));
+//
+//        System.out.println(l1.recSearch(9));
+//
+////        l1.reverse();
+////        l1.print();
+//        l1.deleteNthFromLast(2);
+//        l1.print();
+//
+//        l1.addFirst(1);
+//        l1.addFirst(8);
+//
 
-        l1.print();
-//         add first
+
+        l1.addFirst(1);
         l1.addFirst(2);
-        l1.print();
         l1.addFirst(1);
         l1.print();
-//        add last
-        l1.addLast(3);
-        l1.print();
-        l1.addLast(4);
-        l1.print();
 
-        l1.addMiddle(2, 9);
-        l1.print();
-
-        System.out.println("LL size : " + l1.size);
-
+        System.out.println(l1.isPalindrome());
 
     }
 }
---
+++ src/linkedList/Sample.java
@@ -0,0 +1,26 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+public class Sample {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(3,8);
+        list.addFirst(4);
+        list.addLast(0);
+
+
+        for(int val : list){
+            System.out.print(val + " -> ");
+        }
+        System.out.println("null");
+
+        System.out.println(list.getLast());
+        System.out.println(list.getFirst());
+        System.out.println(list.get(5));
+    }
+}
