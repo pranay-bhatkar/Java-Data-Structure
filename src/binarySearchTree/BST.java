b70ae5da9dc5377a5f5974887f73daf030da2c7d
pranay-bhatkar <pranaybhatkar81@gmail.com>
2026-05-06 16:35:42 +0530

    Binary Search Tree (BST)

--- src/binarySearchTree/BST.java
+++ src/binarySearchTree/BST.java
@@ -1,4 +1,163 @@
 package binarySearchTree;
 
+import java.util.ArrayList;
+
 public class BST {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) return new Node(val);
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) return;
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static boolean search(Node root, int key) {
+        if (root == null) return false;
+
+        if (root.data == key) return true;
+
+        if (root.data > key) return search(root.left, key);
+        else return search(root.right, key);
+
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static Node delete(Node root, int val) {
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case 1 - leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case 2 - single child
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+
+            // case 2 - both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+
+        }
+        return root;
+    }
+
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) return;
+
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        } else if (root.data < k1) {
+            printInRange(root.left, k1, k2);
+        } else {
+            printInRange(root.right, k1, k2);
+        }
+    }
+
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i : path) {
+            System.out.print(i + "->");
+        }
+        System.out.println("N");
+    }
+
+    public static void printRoot2Path(Node root, ArrayList<Integer> path) {
+        if (root == null) return;
+
+        path.add(root.data);
+
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        }
+        printRoot2Path(root.left, path);
+        printRoot2Path(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
+    public static boolean isValidBST(Node root, Node min, Node max) {
+        if (root == null) return true;
+
+        if (min != null && root.data <= min.data) {
+            return false;
+        } else if (max != null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+    public static void main(String[] args) {
+        int[] values = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        Node root = null;
+
+        for (int i : values) {
+            root = insert(root, i);
+        }
+
+        inorder(root);
+        System.out.println();
+
+        if (search(root, 6)) System.out.println("Found");
+        else System.out.println("Not Found");
+
+        System.out.println();
+        root = delete(root, 1);
+        inorder(root);
+
+        System.out.println();
+        printInRange(root, 5, 12);
+
+        System.out.println();
+        System.out.println("----Root to leach node path----");
+        printRoot2Path(root, new ArrayList<>());
+
+        System.out.print("given BST ");
+        if (isValidBST(root, null, null)) {
+            System.out.println("Is valid");
+        } else {
+            System.out.println("not valid");
+        }
+    }
 }
